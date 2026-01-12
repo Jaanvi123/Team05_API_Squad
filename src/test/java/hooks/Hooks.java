2b49507cee7c21a9107866d14232a84f26afc72d
Jaanvi123 <jmattalwar@gmail.com>
2026-01-12 16:28:42 -0600

    changes to config

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,5 +1,21 @@
 package hooks;
 
+import io.cucumber.java.Before; 
+import io.cucumber.java.After; 
+
+
+
 public class Hooks {
 
+	@Before public void setup() 
+	{ 		
+	System.out.println("----- Test Execution Started -----");
+		}
+	
+	
+	@After public void tearDown() 
+	{ 
+		System.out.println("----- Test Execution Completed -----"); 
+	} 
+	
 }
