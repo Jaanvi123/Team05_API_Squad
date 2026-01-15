b7ac7731fa830ea9ddbfaf98b52ca9ec27d547fb
Jaanvi123 <jmattalwar@gmail.com>
2026-01-15 16:44:27 -0600

    token udpated

--- src/test/java/utilities/TokenManager.java
+++ src/test/java/utilities/TokenManager.java
@@ -11,7 +11,7 @@ public class TokenManager {
 
 	// Method to get the token
 	public static String getToken() {
-		token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb2JpbnJhbmphbkBnbWFpbC5jb20iLCJpYXQiOjE3Njg0ODc1NTIsImV4cCI6MTc2ODUxNjM1Mn0.51vjqzhbFdbsjIDJ1PZBegx8gojOpmUh0BmWT8aBubFd1bQUL-c7NW2ixhl60m0clGubXkbvQm3DHeh4O-zp7g";
+		token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb2JpbnJhbmphbkBnbWFpbC5jb20iLCJpYXQiOjE3Njg1MTY5ODMsImV4cCI6MTc2ODU0NTc4M30.7VHGOXnx82czXE0npPuKxl9bDEe3b41Cipuj4O2WCo_uyzZZnaYwyZjUKUyLNYu82DQAaHKyAEuDL43djygXow";
 		return token;
 	}
 
